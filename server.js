8c7e85208a3557bdb21cd988d49f5176d45994bb
prvz786123 <prvz786123@gmail.com>
2018-08-25 01:16:41 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express=require('express');
+const hbs=require('hbs');
+const fs=require('fs');
+
+var app=express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.use(express.static(__dirname+'/public'))
+app.set('view engine','hbs');
+
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt',(text)=>{
+  return text.toUpperCase();
+});
+
+app.use((req,res,next)=>{
+  var now=new Date().toString();
+  var log=`${now} : ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log',log+'\n',(err)=>{
+    if(err){
+      console.log('Unable to append to server.log');
+      
+    }
+  });  
+  next();
+})
+
+
+
+
+app.get('/',(req, res)=>{
+    // res.send('<h1>Hello Express!</h1>')
+    res.render('home.hbs',{
+      pageTitle:"Home Page",
+      welcomeMsg:"Welcome To My Website",
+
+    })
+});
+
+app.get('/about',(req,res)=>{
+  res.render('about.hbs',{
+    pageTitle:'About Page',
+
+  })
+})
+
+app.get('/bad',(req, res)=>{
+  res.render({
+    errorMessage:"Unable to load resources"
+  })
+})
+
+
+
+app.listen(3000,()=>{
+  console.log('Server started on port 3000');
+});
